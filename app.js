c1409c81e608cdaf3c5a92322a1ea0b01d41fb78
dngwoo <akfmzk84@naver.com>
2020-09-06 22:27:11 +0900

    익스프레스 라우터 분리하기

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require('express')
 const app = express()
+const postRouter = require('./routes/post')
 
 app.get('/', (req,res)=>{
     res.send('hello express')
 })
 
-app.post('/api/post', (req,res)=> {
-    res.json([
-        {id: 1, content: 'hello'},
-        {id: 1, content: 'hello2'},
-        {id: 1, content: 'hello3'}
-    ])
-})
+// prefix 처럼 post가 자동으로 붙는다.
+app.use('post', postRouter)
+
 
 app.listen(3065, () => {
     console.log("서버열림")
